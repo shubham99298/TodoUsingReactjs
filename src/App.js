3319a8a820fcc58f0345e60a0e8f0b0ec29b13d8
Shubham <shubhamlata2002@gmail.com>
2023-09-01 17:44:40 +0530

    Todo using ReactJS

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from './comonents/header';
+import Home from './comonents/Home';
+// import Task from './comonents/task';
+
+
+function App() {
+  
+  return(  
+  <div >
+     <Header /> 
+     <Home />
+  </div>
+  );
+}
+
+export default App;
---
+++ src/comonents/Home.jsx
@@ -0,0 +1,70 @@
+import React,{ useEffect, useState }  from 'react';
+import Task from './Task';
+
+const Home = () => {
+  const initilarray =localStorage.getItem("tasks")?JSON.parse(localStorage.getItem("tasks")):[];
+  
+  const[tasks, setTasks]= useState(initilarray);
+  const[title,setTitle]= useState("");
+  const[description,setDescription]= useState("");
+  
+  
+
+  const submitHandler= (e)=> {
+    e.preventDefault();
+
+    setTasks([
+      ...tasks,
+      {
+         title,
+        description,
+        
+      }]);
+      localStorage.setItem("tasks",JSON.stringify(tasks));
+  
+  };
+  
+  const deleteTask=(index)=> {
+    const filteredarr=tasks.filter((val,i)=>{
+      return i!==index;
+    });
+    console.log(filteredarr);
+    setTasks(filteredarr)
+
+  };
+
+  useEffect(()=>{
+    localStorage.setItem("taks",JSON.stringify(tasks));
+
+  },[tasks]);
+  
+  
+
+  return ( 
+     <div className="container">
+     <h1>Daily Goals</h1>
+    <form onSubmit={submitHandler}>
+      <input 
+         type="text"
+         placeholder="Title" 
+         value={title}
+          onChange={(e)=>setTitle(e.target.value)}
+       />
+      <textarea 
+      placeholder="Description"
+      value={description}
+      onChange={(e)=>setDescription(e.target.value)}
+      ></textarea>
+
+      <button type="submit" >ADD</button>
+    </form>
+    
+    {tasks.map((item,index) =>(
+      < Task key={index} title={item.title} description={item.description}
+      deleteTask={deleteTask} index={index}/>
+    ))}
+  </div>
+  );
+}; 
+
+export default Home;
